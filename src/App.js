33aa52aa99e947c6c599b1efc9530af9e03e1849
Evripidis Pantelaios <euripidis_pantelaios@hotmail.com>
2022-10-13 16:29:32 +0300

    Update main component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
+import uuidv4 from 'uuid/v4';
+
+import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
+import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
 
 import './App.css';
 
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
-    { text: 'Do all exercises!', id: 'g1' },
-    { text: 'Finish the course!', id: 'g2' },
+    { text: 'Do all homework', id: uuidv4() },
+    { text: 'Finish the assignment', id: uuidv4() },
+    { text: 'Refactoring code for this repo.', id: uuidv4() },
   ]);
 
   const addGoalHandler = (enteredText) => {
     setCourseGoals((prevGoals) => {
       const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      updatedGoals.unshift({ text: enteredText, id: uuidv4() });
       return updatedGoals;
     });
   };
@@ -23,7 +28,23 @@ const App = () => {
     });
   };
 
-  return <></>;
+  return (
+    <>
+      <section id="goal-form">
+        <CourseInput props={addGoalHandler} />
+      </section>
+      <section id="goals-list">
+        {courseGoals.length > 0 ? (
+          <CourseGoalList
+            items={courseGoals}
+            onDeleteItem={deleteItemHandler}
+          />
+        ) : (
+          <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+        )}
+      </section>
+    </>
+  );
 };
 
 export default App;
