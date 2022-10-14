8ba66075da5431fa92ffc4bc7192667bae844ddb
Evripidis Pantelaios <euripidis_pantelaios@hotmail.com>
2022-10-14 18:43:33 +0300

    refactoring: change how to get some props and className

--- src/components/CourseGoals/GoalInput/ResetLocalStorage.js
+++ src/components/CourseGoals/GoalInput/ResetLocalStorage.js
@@ -1,13 +1,18 @@
 import Button from '../../UI/Button/Button';
 
-const CourseInput = ({ props }) => {
+const CourseInput = (props) => {
   const resetItemsHandler = () => {
     localStorage.clear();
     props.onResetItems();
   };
 
   return (
-    <Button type="button" onClick={resetItemsHandler} props={props}>
+    <Button
+      className={props.className}
+      type={props.type}
+      onClick={resetItemsHandler}
+      props={props}
+    >
       Reset All
     </Button>
   );
