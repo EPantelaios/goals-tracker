841201d2e1ea93db4ac027813b11d5375723edbd
evripant <evripidispantelaios@gmail.com>
2022-10-30 18:38:25 +0200

    add 'button' for default prop of type

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -5,7 +5,7 @@ import styles from './Button.module.css';
 const Button = (props) => {
   return (
     <button
-      type={props.type}
+      type={props.type || 'button'}
       className={`${styles.button} ${props.className}`}
       onClick={props.onClick}
     >
